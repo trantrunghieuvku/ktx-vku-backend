76b7723f2625ce7fa14c3c0c0193b07ef9cc99e7
trantrunghieuvku <trantrunghieua13@gmail.com>
2020-10-21 21:11:29 +0700

    .

---
+++ src/main/java/com/vku/ktx/KtxApplication.java
@@ -0,0 +1,13 @@
+package com.vku.ktx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vku/ktx/KtxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vku.ktx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
